c3ff2bc4acdb5d51b9203b92d49b0aeccab29d76
shourovahmd <moh.abuhanifa@gmail.com>
2022-05-25 15:25:51 +0600

    again

--- index.js
+++ index.js
@@ -1,35 +1,61 @@
-const express = require('express');
-const cors = require('cors');
-const jwt = require('jsonwebtoken');
-require('dotenv').config();
+const express = require("express");
+const cors = require("cors");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const ObjectId = require("mongodb").ObjectId;
 app.use(cors());
 app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.4n3og.mongodb.net/?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
 
-async function run () {
-    try {
-        await client.connect();
-        const productsCollection = client.db('manufacturer_website').collection('products');
-        app.get('/products', async (req, res) => {
-            const products = await productsCollection.find({}).toArray();
-            res.send(products);
-        });
-    } catch (e) {
-        console.error(e);
-    } finally {
-        //client.close();
-    }
-  
-};
+async function run() {
+  try {
+    await client.connect();
+    const productsCollection = client
+      .db("manufacturer_website")
+      .collection("products");
+
+    app.get("/products", async (req, res) => {
+      const products = await productsCollection.find({}).toArray();
+      res.send(products);
+    });
+
+    //adding and posting user/items api
+
+    app.post("/products", async (req, res) => {
+      const newProducts = req.body;
+      console.log("adding new Item", newProducts);
+      const result = await productsCollection.insertOne(newProducts);
+      res.send(result);
+    });
+
+    //  deleting user/items api
+
+    app.delete("/products/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      console.log("deleting Item", id);
+      const result = await productsCollection.deleteOne(query);
+      res.send(result);
+    });
+  } catch (e) {
+    console.error(e);
+  } finally {
+    //client.close();
+  }
+}
 run().catch(console.dir);
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Example app listening on port ${port}`);
 });
