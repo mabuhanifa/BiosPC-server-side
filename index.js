c94702d59951910d8d76ecf046bfa171a4eab291
shourovahmd <moh.abuhanifa@gmail.com>
2022-12-19 01:29:56 +0600

    updated

--- index.js
+++ index.js
@@ -8,7 +8,9 @@ const { MongoClient, ServerApiVersion } = require("mongodb");
 const ObjectId = require("mongodb").ObjectId;
 app.use(cors());
 app.use(express.json());
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.4n3og.mongodb.net/?retryWrites=true&w=majority`;
+
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
